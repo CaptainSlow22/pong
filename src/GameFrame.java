81e4f83e2b04382c965fbcfaf7ea3698f0ad393e
CaptainSlow22 <razvandumitru2002@yahoo.com>
2024-01-18 12:19:17 +0200

    first commit

---
+++ src/GameFrame.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class GameFrame extends JFrame {
+    GamePanel panel;
+    GameFrame(){
+        panel = new GamePanel();
+        this.add(panel);
+        this.setTitle("Pong Game");
+        this.setResizable(false);
+        this.setBackground(new Color(186,186,186));
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
+}
